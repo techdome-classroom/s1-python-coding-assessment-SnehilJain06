5b86cadf51b91207cf6c4f411e4facafe43913ec
SnehilJain06 <snehilj88@gmail.com>
2024-09-30 12:36:13 +0530

    Auto commit on save

--- program1.py
+++ program1.py
@@ -9,15 +9,15 @@ class Solution:
     islands = 0
 
      def dfs(r, c):
-            # Boundary check and stop if it's water or already visited
-            if r < 0 or r >= rows or c < 0 or c >= cols or grid[r][c] == 'W':
-                return
-            # Mark the current land as visited
-            grid[r][c] = 'W'
-            # Recursively visit neighboring cells (up, down, left, right)
-            dfs(r - 1, c)  # up
-            dfs(r + 1, c)  # down
-            dfs(r, c - 1)  # left
-            dfs(r, c + 1)  # right
+        # Boundary check and stop if it's water or already visited
+        if r < 0 or r >= rows or c < 0 or c >= cols or grid[r][c] == 'W':
+            return
+        # Mark the current land as visited
+        grid[r][c] = 'W'
+        # Recursively visit neighboring cells (up, down, left, right)
+        dfs(r - 1, c)  # up
+        dfs(r + 1, c)  # down
+        dfs(r, c - 1)  # left
+        dfs(r, c + 1)  # right
 
     return 0
