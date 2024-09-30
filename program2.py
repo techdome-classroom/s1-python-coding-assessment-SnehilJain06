77a6dd53397b1a031c8e7b15a4969196b25c8be9
SnehilJain06 <snehilj88@gmail.com>
2024-09-30 12:51:04 +0530

    Auto commit on save

--- program2.py
+++ program2.py
@@ -13,12 +13,12 @@ def decode_message( s: str, p: str) -> bool:
                         dp[0][j] = dp[0][j - 1]
 
     # Fill the DP table
-    for i in range(1, m + 1):
-        for j in range(1, n + 1):
-            if p[j - 1] == s[i - 1] or p[j - 1] == '?':
-                dp[i][j] = dp[i - 1][j - 1]  # Character match or '?' matching one character
-            elif p[j - 1] == '*':
-                dp[i][j] = dp[i - 1][j] or dp[i][j - 1]  # '*' matching sequence or empty
+        for i in range(1, m + 1):
+                for j in range(1, n + 1):
+                        if p[j - 1] == s[i - 1] or p[j - 1] == '?':
+                                dp[i][j] = dp[i - 1][j - 1]  # Character match or '?' matching one character
+                        elif p[j - 1] == '*':
+                                dp[i][j] = dp[i - 1][j] or dp[i][j - 1]  # '*' matching sequence or empty
 
     # Return True if the entire string matches the pattern
     if dp[m][n]:
