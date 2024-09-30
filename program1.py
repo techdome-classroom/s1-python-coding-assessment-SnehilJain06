e305093bcd5ccb506b9fb3a08b70641770cd8bc5
SnehilJain06 <snehilj88@gmail.com>
2024-09-30 12:36:49 +0530

    Auto commit on save

--- program1.py
+++ program1.py
@@ -12,9 +12,9 @@ class Solution:
     # Boundary check and stop if it's water or already visited
         if r < 0 or r >= rows or c < 0 or c >= cols or grid[r][c] == 'W':
             return
-        # Mark the current land as visited
-        grid[r][c] = 'W'
-        # Recursively visit neighboring cells (up, down, left, right)
+    # Mark the current land as visited
+    grid[r][c] = 'W'
+    # Recursively visit neighboring cells (up, down, left, right)
         dfs(r - 1, c)  # up
         dfs(r + 1, c)  # down
         dfs(r, c - 1)  # left
